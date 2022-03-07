0f5c8da6cb0ec670f3b81e8be2be2d2f2af04289
Shahosol <soltani.shaho@gmail.com>
2022-03-07 20:14:39 +0200

    redux weather app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchWeatherAction } from './redux/slices/weatherSlices';
+
 
 function App() {
+  const [city, setCity] = useState('seoul');
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    dispatch(fetchWeatherAction('seoul'));
+  }, [city]);
+  
+  const state = useSelector(state => state.weather);
+  const { loading, weather, error } = state || {};
+
+  if(!weather){
+    return null
+  }
+
+  console.log(weather);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Weather search</h1>
+        <h2>Default location is Seoul</h2>
+      <input
+        className='input'
+        type="text"
+        value={city}
+        onClick={() => dispatch(fetchWeatherAction(city))}
+        onChange={event => setCity(event.target.value)}
+      />
+    <br />
+    <br />
+        <button onClick={() => dispatch(fetchWeatherAction(city))}>Search</button>
+        {weather.map((weatherItem, index) => (
+        <div className='info-box' key={index}>
+          <p>State of sky: {`${weatherItem.description}`}</p>
+          <p>Weather: {`${weatherItem.main}`}</p>
+        </div>
+      ))}
       </header>
     </div>
   );
